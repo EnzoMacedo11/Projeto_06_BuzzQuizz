537498a3a65678ce5ce2f5c19aae738a2c64378e
calcado <pecalcado@gmail.com>
2022-09-03 16:55:46 -0300

    versao teste

--- scripts.js
+++ scripts.js
@@ -1,38 +1,47 @@
-function checarMeuQuizz(){
-    for (i = 0; i < ul.lenght; i++){
-        if(i >= 1){
-          let divSemQuizz =  document.QuerySelector(".p-semQuizz");
-          let divComSeuQUizz = document.QuerySelector("p-divComSeuQUizz ")
-          divSemQuizz.classList.add("p-hidden")
-          divComSeuQUizz.classList.remove("p-hidden")
-        }
-    }
-}
-checarMeuQuizz();
+alert('Ta funcionando')
 
-let todosQuizzes = {};
+let todosQuizzes = [];
 
+buscarQuizzes()
 function buscarQuizzes(){
     let todosQuizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    todosQuizzes.then(renderizarQuizzes);
+    todosQuizzes.then(renderizarQuizz);
     todosQuizzes.catch(tratarErro);
+    alert('Tá indo')
       
 }
 
-buscarQuizzes();
-
-function renderizarQuizzes(quizz){
-    const ulQuizzOutros = document.QuerySelector('p-listaQuizzOutros');
-    ulQuizzOutros.innerHTML = '';
-    for(j = 0; j < quizz.lenght; j++ ){
-    ulQuizzOutros.innerHTML += `<li class="p-Quizz" onclick="selecionarQuizz(${quizz.id[j]})"> 
-        <span class="tituloQuizz">${quizz.tittle[j]}</span>
-        <img src="${quizz.image[j]}>" </li>`
-    }
 
 
-}
-renderizarQuizzes();
+// function identificarQuizz(resposta){
+//     todosQuizzes = []
+//     const todosQuizzes = resposta.data
+// }
+
+
+// const meuQuizz = quizzesServidor.filter(identificarQuizz);
+
+function renderizarQuizz(quizz){
+    alert('Tamo indo')
+    console.log(quizz)
+//     ulQuizzOutros = '';
+//     ulQuizzOutros = quizz.data;
+   
+//     let ulQuizzOutros = document.QuerySelector('p-listaQuizzOutros');
+//     ulQuizzOutros.innerHTML += quizzesServidor;
+
+//     for(i = 0; i < quizz.data.lenght; i++ ){
+//     ulQuizzOutros.innerHTML += `<li class="p-Quizz" onclick="selecionarQuizz"(${quizz.data.id[i]})"> 
+//         <span class="tituloQuizz">${quizz.data.tittle[i]}</span>
+//         <img src="${quizz.data.image[i]}"> </li>`
+//     }
+
+
+// }
+
+
+
+
 
 function selecionarQuizz(){
    let quizz = document.querySelector('.p-Quizz')
@@ -50,3 +59,15 @@ function tratarErro(erro){
     const statusCode = erro.response.status;
     console.log(statusCode);
 }
+
+function checarMeuQuizz(){
+    for (i = 0; i < ul.lenght; i++){
+        if(i >= 1){
+          let divSemQuizz =  document.QuerySelector(".p-semQuizz");
+          let divComSeuQUizz = document.QuerySelector("p-divComSeuQUizz ")
+          divSemQuizz.classList.add("p-hidden")
+          divComSeuQUizz.classList.remove("p-hidden")
+        }
+    }
+}
+checarMeuQuizz();
